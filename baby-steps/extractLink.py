139ad25aa2477e9784900ead0c8e8230ca9d3e1e
enlighter <enlighter.1992@gmail.com>
2015-03-21 03:53:10 +0530

    [baby steps] add alternative solution to extractLink

--- baby-steps/extractLink.py
+++ baby-steps/extractLink.py
@@ -12,7 +12,15 @@ page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">')
 start_link = page.find('<a href=')
 urlBeginning = page[start_link:].find('"');
+#alternatively:
+#start_quote = page.find('"', start_link)
+
 urlEnding = page[start_link + urlBeginning + 1:].find('"')
+#alternatively:
+#end_quote = page.find('"', start_quote + 1)
 
 url = page[start_link + urlBeginning + 1: start_link + urlBeginning + urlEnding + 1]
+#alternatively:
+#url = page[start_quote + 1: end_quote]
+
 print(url)
