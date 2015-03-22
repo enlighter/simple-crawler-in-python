a20d015cefe6c4dad63dec51c266f05ac5993a94
enlighter <enlighter.1992@gmail.com>
2015-03-22 19:28:33 +0530

    add method for finding 2nd occurrence of a string

---
+++ baby-steps/findSecondOccurrence.py
@@ -0,0 +1,21 @@
+# Define a procedure, find_second, that takes
+# two strings as its inputs: a search string
+# and a target string. It should return a
+# number that is the position of the second
+# occurrence of the target string in the
+# search string.
+def find_second(target, search):
+    second = target.find(search, target.find(search) + 1)
+    return second
+
+
+
+
+
+danton = "De l'audace, encore de l'audace, toujours de l'audace"
+print find_second(danton, 'audace')
+#>>> 25
+
+twister = "she sells seashells by the seashore"
+print find_second(twister,'she')
+#>>> 13
