0d67ee3c5063e7b9479292ed58c07000b2585d39
enlighter <enlighter.1992@gmail.com>
2015-04-01 21:15:45 +0530

    create a very rudimentary next day method

--- baby-steps/daysBetweenDates.py
+++ baby-steps/daysBetweenDates.py
@@ -7,6 +7,32 @@
 # time travel). 
 #
 
+###
+### Define a simple nextDay procedure, that assumes
+### every month has 30 days.
+###
+### For example:
+###    nextDay(1999, 12, 30) => (2000, 1, 1)
+###    nextDay(2013, 1, 30) => (2013, 2, 1)
+###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
+###
+
+def nextDay(year, month, day):
+    """
+    Returns the year, month, day of the next day.
+    Simple version: assume every month has 30 days.
+    """
+    # YOUR CODE HERE
+    day += 1
+    if day == 31:
+      day = 1
+      month += 1
+      if month == 13:
+        month = 1
+        year += 1
+
+    return (year, month, date)
+
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     ##
     # Your code here.
