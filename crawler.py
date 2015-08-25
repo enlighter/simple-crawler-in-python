c06504b05f88f9c22791876516bdf70e68676075
enlighter <mandal.sushovan92@gmail.com>
2015-08-25 22:04:41 +0530

    implement full-depth link crawl from the seed page

--- crawler.py
+++ crawler.py
@@ -1,2 +1,20 @@
 ### This project is built for compatibilty with Python 3.4 (tested and built using python 3.4.3) ###
+from baby_steps.extractLinks import get_all_links
+import requests
 
+def crawl_web(seed_page):
+    tocrawl = []
+    crawled = []
+    tocrawl.append(seed_page)
+
+    while tocrawl:
+        dest = tocrawl.pop()
+        if dest not in crawled:
+            current_page = requests.get(dest)
+            links_extracted = get_all_links(current_page.content.decode('UTF-8'))
+            tocrawl.extend(links_extracted)
+            crawled.append(dest)
+        else:
+            continue
+
+    return crawled
--- tester.py
+++ tester.py
@@ -1,7 +1,25 @@
+### This project is built for compatibilty with Python 3.4 (tested and built using python 3.4.3) ###
+
 from baby_steps.extractLinks import get_all_links
 import requests
+from crawler import crawl_web
 
-url = "https://www.udacity.com/cs101x/index.html"
-response = requests.get(url)
-links = get_all_links(response.content.decode('UTF-8'))
-print(links)
+def test_link_extraction():
+    url = "https://www.udacity.com/cs101x/index.html"
+    response = requests.get(url)
+    print(response.content)
+    links = get_all_links(response.content.decode('UTF-8'))
+    print(links)
+
+def test_requests():
+    url = "https://blockchain.info/address/155VGwiUkDoMWmZYE5FuuuqkppS6tf5SNr"
+    response = requests.get(url)
+    print(response.content)
+
+def test_crawler():
+	crawled = crawl_web("https://www.udacity.com/cs101x/index.html")
+	print(crawled)
+
+#test_link_extraction()
+#test_requests()
+test_crawler()
